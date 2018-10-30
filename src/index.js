fe4288cefe9da5335729ac9bc33ba83b946b00bd
Julian Salinas <julian4001@outlook.com>
2018-10-30 12:55:35 -0600

    Bugfix:  Page didn't load

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import 'mdbreact/dist/css/mdb.css';
 import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css'; 
 
-import App from './App';
+import App from './app';
 import registerServiceWorker from './service-worker';
 import { BrowserRouter} from 'react-router-dom';
 
